ef47e3bf31575aab50dc1569d382db633d12262f
Ichsan Firdaus <ichsanfirdaus99@gmail.com>
2016-03-19 16:18:37 +0700

    Tidy Up and Add Some Include

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,13 +2,13 @@
 <html>
 <head>
 	<meta name="csrf-token" content="{{ csrf_token() }}">
-
 	<title>Laravel Ajax</title>
 
 	<link rel="stylesheet" type="text/css" href="{{ url('assets/css/bootstrap.min.css') }}">
 
 	<script src="{{ url('assets/js/jquery.min.js') }}"></script>
 	<script src="{{ url('assets/js/bootstrap.min.js') }}"></script>
+	@include('student.js')
 </head>
 <body>
 	<nav class="navbar navbar-default">
@@ -22,7 +22,6 @@
 				</button>
 				<a href="#" class="navbar-brand">LARAJAX</a>
 			</div>
-
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			</div>
 		</div>
--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -15,7 +15,7 @@
 					<th>Pilihan</th>
 				</tr>
 			</thead>
-			<tbody>
+			<tbody id="table_body">
 				<?php $no = 1; ?>
 				@foreach($student as $value)
 					<tr>
@@ -36,111 +36,4 @@
 	@include('student.modalcreate')
 	@include('student.modaledit')
 	@include('student.modaldelete')
-
-
-
-	<script type="text/javascript">
-
-		function reloadSiswa() {
-			$.ajax({
-				method: "GET",
-				url: "{{ url('siswa') }}",
-				data: {},
-				success: function(data) {
-					window.history.pushState('', '', '{{ url('siswa') }}');
-					document.open();
-					document.write(data);
-					document.close();
-				}
-			});
-		}
-
-		function editSiswa(id) {
-			$.ajax({
-				method: "GET",
-				url: "{{ url('siswa') }}"+ '/' + id,
-				data: {},
-				success: function(data) {
-					$('#form_edit').attr("action", "{{ url('siswa/update') }}" + '/' + data.id);
-					$('#edit_id').val(data.id);
-					$('#edit_nis').val(data.nis);
-					$('#edit_nama').val(data.nama);
-					$('#edit_kelas').val(data.kelas);
-				}
-			});
-		}
-
-		function hapusSiswa(id) {
-			$.ajax({
-				method: "GET",
-				url: "{{ url('siswa') }}"+ '/' + id,
-				data: {},
-				success: function(data) {
-					$('.judul-modal').html(data.nama);
-					$('.id_hapus').val(data.id);
-				}
-			});
-		}
-// ----------------------------------------------------------------------------------
-		$(function() {
-			$('#hapus_siswa').click(function(e) {
-				e.preventDefault();
-
-				var hapus = $('#data_hapus').val();
-
-				$.ajax({
-					method: "GET",
-					url: "{{ url('siswa/delete') }}" + '/' + hapus,
-					data: {},
-					success: function(data) {
-						reloadSiswa();
-					},
-					error: function() {
-						alert('ajax error');
-					}
-				});
-			})
-
-			$('#form_create').submit(function(e) {
-				e.preventDefault();
-
-				var data_nis = $('#nis').val();
-				var data_nama = $('#nama').val();
-				var data_kelas = $('#kelas').val();
-
-				$.ajax({
-					method: "POST",
-					url: "{{ url('siswa/create') }}",
-					data: { nis: data_nis, nama: data_nama, kelas: data_kelas },
-					success: function() {
-						reloadSiswa();
-					},
-					error: function() {
-						alert('ajax error');
-					}
-				});
-			})
-
-			$('#form_edit').submit(function (e){
-				e.preventDefault();
-
-				var data_id = $('#edit_id').val();
-				var data_nis = $('#edit_nis').val();
-				var data_nama = $('#edit_nama').val();
-				var data_kelas = $('#edit_kelas').val();
-
-				$.ajax({
-					data: { id: data_id, nis: data_nis, nama: data_nama, kelas: data_kelas },
-					method: "POST",
-					url: "{{ url('siswa/update') }}",
-					success: function(data) {
-						reloadSiswa();
-					},
-					error: function() {
-						alert('ajax error');
-					}
-				});
-			})
-		})
-	</script>
 @stop
